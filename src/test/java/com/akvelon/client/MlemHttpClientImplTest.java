ea82ddef28a4503a034d91b254d4d8c72913f81a
Aleksandr Dudko <aleksandr.dudko@akvelon.com>
2022-08-04 18:01:23 +0400

    MLOPS-3: Add the exception handling for some tests.

--- src/test/java/com/akvelon/client/MlemHttpClientImplTest.java
+++ src/test/java/com/akvelon/client/MlemHttpClientImplTest.java
@@ -18,11 +18,11 @@ import java.util.stream.Collectors;
 
 public class MlemHttpClientImplTest {
     private final ObjectMapper mapper = new ObjectMapper();
-    private final static String host = "http://example-mlem-get-started-app.herokuapp.com/";
+    private final static String HOST_URL = "http://localhost:8080/";
 
-    private static ExecutorService executorService = Executors.newFixedThreadPool(10);
-    private final MlemHttpClientImpl clientWithExecutor = new MlemHttpClientImpl(executorService, host);
-    private final MlemHttpClientImpl clientWithOutExecutor = new MlemHttpClientImpl(host);
+    private final static ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private final MlemHttpClientImpl clientWithExecutor = new MlemHttpClientImpl(executorService, HOST_URL);
+    private final MlemHttpClientImpl clientWithOutExecutor = new MlemHttpClientImpl(HOST_URL);
 
     private final String dataRequestBody = "{\n" +
             "  \"data\": {\n" +
@@ -66,22 +66,19 @@ public class MlemHttpClientImplTest {
     @Test
     @DisplayName("Test get /interface.json method with string response")
     public void testGetInterfaceString() throws ExecutionException, InterruptedException {
-        CompletableFuture<String> future = clientWithExecutor.interfaceStringAsync();
-        assertResponseFuture(future);
+        assertResponseStringOrHandleException(clientWithExecutor.interfaceStringAsync());
     }
 
     @Test
     @DisplayName("Test get /interface.json method without executor")
     public void testGetInterfaceWithoutExecutor() throws ExecutionException, InterruptedException {
-        CompletableFuture<String> future = clientWithOutExecutor.interfaceStringAsync();
-        assertResponseFuture(future);
+        assertResponseStringOrHandleException(clientWithOutExecutor.interfaceStringAsync());
     }
 
     @Test
     @DisplayName("Test get /interface.json method with json response")
     public void testGetInterfaceJson() throws ExecutionException, InterruptedException {
-        CompletableFuture<JsonNode> future = clientWithExecutor.interfaceJsonAsync();
-        assertResponseJsonNode(future);
+        assertResponseJsonOrHandleException(clientWithExecutor.interfaceJsonAsync());
     }
 
     @Test
@@ -96,29 +93,25 @@ public class MlemHttpClientImplTest {
     @Test
     @DisplayName("Test post /predict method with string response")
     public void testPredictString() throws ExecutionException, InterruptedException {
-        CompletableFuture<String> future = clientWithExecutor.predictAsync(dataRequestBody);
-        assertResponseFuture(future);
+        assertResponseStringOrHandleException(clientWithExecutor.predictAsync(dataRequestBody));
     }
 
     @Test
     @DisplayName("Test post /predict method with JSON response")
     public void testPredictJson() throws ExecutionException, InterruptedException {
-        CompletableFuture<JsonNode> future = clientWithExecutor.predictAsync(stringToJsonNode(dataRequestBody));
-        assertResponseJsonNode(future);
+        assertResponseJsonOrHandleException(clientWithExecutor.predictAsync(stringToJsonNode(dataRequestBody)));
     }
 
     @Test
     @DisplayName("Test post /predictProba method with STRING request and response")
     public void testPredictProbaString() throws ExecutionException, InterruptedException {
-        CompletableFuture<String> future = clientWithExecutor.predictProbaAsync(dataRequestBody);
-        assertResponseFuture(future);
+        assertResponseStringOrHandleException(clientWithExecutor.predictProbaAsync(dataRequestBody));
     }
 
     @Test
     @DisplayName("Test post /predictProba method with JSON request and response")
     public void testPredictProbaJson() throws ExecutionException, InterruptedException {
-        CompletableFuture<JsonNode> future = clientWithExecutor.predictProbaAsync(stringToJsonNode(dataRequestBody));
-        assertResponseJsonNode(future);
+        assertResponseJsonOrHandleException(clientWithExecutor.predictProbaAsync(stringToJsonNode(dataRequestBody)));
     }
 
     @Test
@@ -135,25 +128,25 @@ public class MlemHttpClientImplTest {
     @Test()
     @DisplayName("Test post /sklearnPredict method with STRING request and response")
     public void testSklearnPredictString() throws InterruptedException, ExecutionException {
-        assertResponseFuture(clientWithExecutor.sklearnPredictAsync(xRequestBody));
+        assertResponseStringOrHandleException(clientWithExecutor.predictProbaAsync(xRequestBody));
     }
 
     @Test
     @DisplayName("Test post /sklearnPredictProba method with STRING request and response")
     public void testSklearnPredictProbaString() throws InterruptedException, ExecutionException {
-        assertResponseFuture(clientWithExecutor.sklearnPredictProbaAsync(xRequestBody));
+        assertResponseStringOrHandleException(clientWithExecutor.sklearnPredictProbaAsync(xRequestBody));
     }
 
     @Test()
     @DisplayName("Test post /sklearnPredict method with JSON request and response")
     public void testSklearnPredictJson() throws InterruptedException, ExecutionException {
-        assertResponseJsonNode(clientWithExecutor.sklearnPredictAsync(stringToJsonNode(xRequestBody)));
+        assertResponseJsonOrHandleException(clientWithExecutor.sklearnPredictAsync(stringToJsonNode(xRequestBody)));
     }
 
     @Test
     @DisplayName("Test post /sklearnPredictProba method with JSON request and response")
     public void testSklearnPredictProbaJson() throws InterruptedException, ExecutionException {
-        assertResponseJsonNode(clientWithExecutor.sklearnPredictProbaAsync(stringToJsonNode(xRequestBody)));
+        assertResponseJsonOrHandleException(clientWithExecutor.sklearnPredictProbaAsync(stringToJsonNode(xRequestBody)));
     }
 
     @Test()
@@ -187,10 +180,10 @@ public class MlemHttpClientImplTest {
     @Test
     @DisplayName("Test post /predict method with executorService = null")
     public void testGetInterfaceExecutorNull() throws ExecutionException, InterruptedException {
-        MlemHttpClientImpl mlemHttpClientImpl = new MlemHttpClientImpl(null, "http://example-mlem-get-started-app.herokuapp.com/");
+        MlemHttpClientImpl mlemHttpClientImpl = new MlemHttpClientImpl(null, HOST_URL);
 
         CompletableFuture<String> future = mlemHttpClientImpl.predictAsync(dataRequestBody);
-        assertResponseFuture(future);
+        assertResponseStringOrHandleException(future);
     }
 
     @Test
@@ -198,22 +191,30 @@ public class MlemHttpClientImplTest {
     public void testListRequests() throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-        MlemHttpClientImpl mlemHttpClientImpl = new MlemHttpClientImpl(executorService, "http://example-mlem-get-started-app.herokuapp.com/");
+        MlemHttpClientImpl mlemHttpClientImpl = new MlemHttpClientImpl(executorService, HOST_URL);
         List<String> dataRequestList = Arrays.asList(dataRequestBody, dataRequestBody, dataRequestBody);
         List<CompletableFuture<String>> completableFutures = dataRequestList.stream()
                 .map(mlemHttpClientImpl::predictAsync)
                 .collect(Collectors.toList());
 
         for (CompletableFuture<String> future : completableFutures) {
-            assertResponseFuture(future);
+            assertResponseStringOrHandleException(future);
         }
 
         executorService.shutdown();
     }
 
-    private void assertResponseFuture(CompletableFuture<String> future) throws ExecutionException, InterruptedException {
+    private void assertResponseStringOrHandleException(CompletableFuture<String> future) throws ExecutionException, InterruptedException {
         Assertions.assertNotNull(future);
-        String response = future.get();
+        String response = future.exceptionally(throwable -> {
+            RestException restException = (RestException) throwable.getCause();
+            assertResponseException(restException);
+            return null;
+        }).get();
+        if (response == null) {
+            return;
+        }
+
         assertResponseString(response);
     }
 
@@ -222,9 +223,19 @@ public class MlemHttpClientImplTest {
         Assertions.assertFalse(response.isEmpty());
     }
 
-    private void assertResponseJsonNode(CompletableFuture<JsonNode> future) throws ExecutionException, InterruptedException {
+    private void assertResponseJsonOrHandleException(CompletableFuture<JsonNode> future) throws ExecutionException, InterruptedException {
         Assertions.assertNotNull(future);
-        JsonNode response = future.get();
+
+        JsonNode response = future.exceptionally(throwable -> {
+            RestException restException = (RestException) throwable.getCause();
+            assertResponseException(restException);
+            return null;
+        }).get();
+
+        if (response == null) {
+            return;
+        }
+
         Assertions.assertNotNull(response);
         Assertions.assertFalse(response.isEmpty());
     }
